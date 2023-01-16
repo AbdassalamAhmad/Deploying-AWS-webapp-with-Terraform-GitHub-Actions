e164f5485f753e77c8f919161c6d9f157e43646f
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-01-16 07:19:49 +0200

    to differ 2nd instance from 1st instance

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -74,14 +74,14 @@ resource "aws_instance" "application_instance_2" {
 
 
 
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   # Use this for your user data (script from top to bottom)
-  #   # install httpd (Linux 2 version)
-  #   yum update -y
-  #   yum install -y httpd
-  #   systemctl start httpd
-  #   systemctl enable httpd
-  #   echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-  #   EOF
+  user_data = <<-EOF
+    #!/bin/bash
+    # Use this for your user data (script from top to bottom)
+    # install httpd (Linux 2 version)
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Hello World from $(hostname -f)<br> This is The 2nd EC2 Instance</h1>" > /var/www/html/index.html
+    EOF
 }
